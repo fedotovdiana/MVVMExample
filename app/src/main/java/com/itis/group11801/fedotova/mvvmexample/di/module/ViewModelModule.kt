944864c2744597e5ec58c510e4b092f2aa9d7398
diana_fedotova <diana.fedotova.2000@mail.ru>
2020-04-12 21:37:08 +0300

    VM di fixed

--- app/src/main/java/com/itis/group11801/fedotova/mvvmexample/di/module/ViewModelModule.kt
+++ app/src/main/java/com/itis/group11801/fedotova/mvvmexample/di/module/ViewModelModule.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.itis.group11801.fedotova.mvvmexample.viewmodel.ViewModelFactory
 import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 abstract class ViewModelModule {
 
     @Binds
+    @Singleton
     abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 }
